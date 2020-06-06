a2a09f74beb5dad3163bfcc4fc1084fd7af28172
jingw2 <jingwangtju@gmail.com>
2020-06-06 11:11:33 +0800

    20200606 Update 1. create setup.py and LICENSE file to create pypi

---
+++ psoco/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='psoco',
+      version='0.0.1',
+      description='partical swarm optimization constraint optimization solver',
+      url='https://github.com/jingw2/solver/tree/master/psoco',
+      author='Jing Wang',
+      author_email='jingw2@foxmail.com',
+      license='MIT',
+      packages=find_packages(),
+      python_requires='>=3.6')
