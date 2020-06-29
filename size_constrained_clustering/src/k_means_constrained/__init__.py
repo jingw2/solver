35cbeb4d93b0be5130dc3c250e556cbf28b610b4
jingw2 <jingwangtju@gmail.com>
2020-06-29 22:28:54 +0800

    Delete size constrained clustering, create new repo

--- size_constrained_clustering/src/k_means_constrained/__init__.py
+++
@@ -1,5 +0,0 @@
-
-__all__ = ['KMeansConstrained']
-
-from .k_means_constrained_ import KMeansConstrained
-
--- size_constrained_clustering/src/sklearn_import/utils/fixes.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from sklearn_import.fixes import _parse_version
-
-np_version = _parse_version(np.__version__)
-
-
-def sparse_min_max(X, axis):
-    return (X.min(axis=axis).toarray().ravel(),
-            X.max(axis=axis).toarray().ravel())
