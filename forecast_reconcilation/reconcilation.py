251867e53c92a198fa1011496e3fd2cb0890a09a
jingw2 <jingw2@foxmail.com>
2020-09-25 17:21:09 +0800

    Update reconcilation.py

--- forecast_reconcilation/reconcilation.py
+++ forecast_reconcilation/reconcilation.py
@@ -34,7 +34,27 @@ def get_summing_matrix(tree: HierarchyTree):
     dfs(mat, tree.root)
     return mat
 
-def top_down(forecasts: dict, tree: HierarchyTree, method="avg_hist_prop"):
+def get_forecast_prop(forecasts_dict: dict, tree: HierarchyTree):
+    queue = [tree.root]
+    props = {"root": 1}
+    while queue:
+        node = queue.pop(0)
+        if len(node.children) == 0:
+            continue
+        s = sum([forecasts_dict[child.name][0] for child in node.children])
+        for child in node.children:
+            ratio = forecasts_dict[child.name][0] / s 
+            props[child.name] = props[node.name] * ratio
+            queue.append(child)
+    p = [props[node.name] for node in tree.bottom]
+    p = np.asarray(p).reshape((-1, 1))
+    return p 
+
+def top_down(forecasts: pd.DataFrame, 
+        history: pd.DataFrame,
+        tree: HierarchyTree, 
+        horizon: int = 7,
+        method="avg_hist_prop"):
     '''
     Top down method
     从上至下拆分
@@ -42,8 +62,44 @@ def top_down(forecasts: dict, tree: HierarchyTree, method="avg_hist_prop"):
             p_j = 1 / T * \sum_{t=1}^T y_{j, t} / y_t
         2. 按照历史平均比例: Proportions of Historical Average, prop_hist_avg
         3. 按预测比例: Forecast Proportions, forecast_prop
+    
+    forecasts and history format like this:
+           | date     | root | series1 | series_2 | series1_sku1 | series2_sku1 |
+           | 20200922 | 1000 | 200     | 300      | 100          | 250          |
+        date is index, pivot table
     '''
-    raise NotImplementedError
+    nodenames = list(tree.nodes.keys())
+    S = get_summing_matrix(tree)
+    history.sort_index(inplace=True) # sort dates ascending
+    history = history[-horizon:]
+    history = df_to_array(history, nodenames)
+    dates = forecasts.index.tolist()
+
+    forecasts_dict = forecasts.to_dict(orient="list")
+    forecasts = df_to_array(forecasts, nodenames)
+    bottom_ids = [nodenames.index(bot.name) for bot in tree.bottom]
+    root_id = nodenames.index("root")
+
+    y_root = forecasts[root_id].reshape((1, -1))
+    if method == "avg_hist_prop":
+        p = np.mean(history[bottom_ids] / history[root_id], axis=1)
+        p /= p.sum() # re-standardize
+        p = p.reshape((-1, 1))
+    if method == "prop_hist_avg":
+        p = np.mean(history[bottom_ids], axis=1) / np.mean(history[root_id])
+        p /= p.sum() # re-standardize
+        p = p.reshape((-1, 1))
+    if method == "forecast_prop":
+        p = get_forecast_prop(forecasts_dict, tree)
+    
+    y = S @ p @ y_root
+    results = pd.DataFrame(y, columns=dates)
+    results["id"] = nodenames
+    cols = [c for c in results.columns.tolist() if c != "id"]
+    results = pd.melt(results, id_vars=["id"], value_vars=cols)
+    results.columns = ["id", "date", "ypred_new"]
+    return results
+
 
 def bottom_up(forecasts: pd.DataFrame, tree: HierarchyTree):
     '''
@@ -56,9 +112,13 @@ def bottom_up(forecasts: pd.DataFrame, tree: HierarchyTree):
     num_bottom_level = tree.num_bottom_level
     bottom_pred = ypred[-num_bottom_level:, :]
     y = S @ bottom_pred
-    results = {}
-    for idx, name in enumerate(nodenames):
-        results[name] = y[idx]
+    
+    dates = forecasts.index.tolist()
+    results = pd.DataFrame(y, columns=dates)
+    results["id"] = nodenames
+    cols = [c for c in results.columns.tolist() if c != "id"]
+    results = pd.melt(results, id_vars=["id"], value_vars=cols) 
+    results.columns = ["id", "date", "ypred_new"]
     return results
 
 def optimal_reconcilation(forecasts: pd.DataFrame, tree: HierarchyTree, method="ols", 
@@ -83,6 +143,11 @@ def optimal_reconcilation(forecasts: pd.DataFrame, tree: HierarchyTree, method="
         4. mint: W_h = k_h W_1, W_1 sample/residual covariance, 样本协方差矩阵，也可以用残差协方差矩阵
             the number of bottom-level series is much larger than T, so shrinkage covariance to 
             diagnoal
+    
+    forecasts format like this:
+           | date     | all | series1 | series_2 | series1_sku1 | series2_sku1 |
+           | 20200922 | 1000| 200     | 300      | 100          | 250          |
+        date is index, pivot table
     '''
     nodenames = list(tree.nodes.keys())
     num_nodes = tree.num_nodes
@@ -113,7 +178,10 @@ def optimal_reconcilation(forecasts: pd.DataFrame, tree: HierarchyTree, method="
     y = coef @ ypred
 
     results = pd.DataFrame(y, columns=dates)
-    results["id"] = nodenames 
+    results["id"] = nodenames
+    cols = [c for c in results.columns.tolist() if c != "id"]
+    results = pd.melt(results, id_vars=["id"], value_vars=cols) 
+    results.columns = ["id", "date", "ypred_new"]
     return results
 
 def df_to_array(forecasts, nodenames):
@@ -142,7 +210,6 @@ def example():
     series = [s for s in series if s != "all"]
     skus = data.loc[~data["sku"].isna(), ["series", "sku"]].drop_duplicates()
     skus = (skus["series"] + "_" + skus["sku"]).tolist()
-    
     # 因为stores就1个，就作为root
     total = {"root": series} # root对应层，是第一层
     skus_h = {k: [v for v in skus if v.startswith(k)] for k in series}
@@ -172,30 +239,34 @@ def example():
         y = np.array(y).ravel()
         ypred = np.array(ypred).ravel()
         return np.abs(y-ypred) / y
+    
+    def preprocess(df):
+        df.fillna("", inplace=True)
+        df.loc[:, "id"] = df.loc[:, "series"] + "_" + df.loc[:, "sku"]
+        df["residual"] = mape(df["y"], df["ypred"])
+        return df 
 
+    train_data = data[data["flag"] == "val"] # to be changed 
     val_data = data[data["flag"] == "val"]
-    val_data.fillna("", inplace=True)
-    val_data.loc[:, "id"] = val_data.loc[:, "series"] + "_" + val_data.loc[:, "sku"]
-    val_data["residual"] = mape(val_data["y"], val_data["ypred"])
+    val_data = preprocess(val_data)
+    train_data = preprocess(train_data)
+
     forecasts = pd.pivot_table(val_data, values=["ypred"], index=["date"], columns=["id"])
-    ytrue = pd.pivot_table(val_data, values=["y"], index=["date"], columns=["id"])
     residuals = pd.pivot_table(val_data, values=["residual"], index=["date"], columns=["id"])
+    history = pd.pivot_table(train_data, values=["y"], index=["date"], columns=["id"])
     forecasts.columns = clear_ids(forecasts.columns)
     residuals.columns = clear_ids(residuals.columns)
+    history.columns = clear_ids(history.columns)
     val_data["id"] = clear_ids(val_data["id"])
-    res = optimal_reconcilation(forecasts, tree, method="mint", residuals=residuals)
-    
-    # rearrange and show comparison between origin and reconcilated
-    cols = [c for c in res.columns.tolist() if c != "id"]
-    res = pd.melt(res, id_vars=["id"], value_vars=cols)
-    res.columns = ["id", "date", "ypred_new"]
-    res = pd.merge(res, val_data[["id", "y", "ypred"]], how="left", on="id")
+
+    # res = optimal_reconcilation(forecasts, tree, method="mint", residuals=residuals)
+    res = top_down(forecasts, history, tree, method="prop_hist_avg")
+    res = pd.merge(res, val_data[["id", "y", "ypred", "date"]], how="left", on=["id", "date"])
     res.loc[res["id"] == "root", "id"] = "all"
     res["mape"] = mape(res["y"], res["ypred"])
     res["mape_new"] = mape(res["y"], res["ypred_new"])
     res[["series", "sku"]] = res["id"].str.split("_", expand=True)
     res.drop(columns=["id"], inplace=True)
-    
     return res 
 
 if __name__ == "__main__":
